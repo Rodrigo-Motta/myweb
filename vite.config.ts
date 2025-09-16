385244d6af57ed0884abef8dab63939b2746eda6
rodrigo <rodrigodamottacc@gmail.com>
2025-09-15 21:05:13 -0300

    fixing images

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,25 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE || '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  const fallbackBase = isDev ? '/' : './';
+
+  return {
+    base: process.env.VITE_BASE || fallbackBase,
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' &&
+      componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
